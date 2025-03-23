f367c5754c1e493e10dc4cc3c125eae5115383fe
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-03-23 17:45:41 +0600

    font family added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 
@@ -7,6 +7,10 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+const poppins = Poppins({
+  weight: ["400", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -14,10 +18,16 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: {
-    default :"Learning Nextjs",
-    template : '%s || Learning Nextjs'
+    default: "Learning Nextjs",
+    template: "%s || Learning Nextjs",
   },
-  keywords:['NextJS' , 'React', 'JavaScript' ,'Learning Next Js' , 'Explorying NextJS'],
+  keywords: [
+    "NextJS",
+    "React",
+    "JavaScript",
+    "Learning Next Js",
+    "Explorying NextJS",
+  ],
   description: "Traying to learning nextJS as best as we can",
 };
 
@@ -25,7 +35,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className}  antialiased`}
       >
         <Navbar></Navbar>
         <main className="min-h-screen  container mx-auto px-10">
