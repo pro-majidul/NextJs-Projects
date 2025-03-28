2a173891f7b480085b6ba13e03b348e38f3f7f8a
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-03-28 23:46:03 +0600

    FAQ section added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,30 +3,64 @@ import React from 'react';
 
 const contact = () => {
     return (
-        <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 ">
-            <div className="flex flex-col justify-between">
-                <div className="space-y-2">
-                    <h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's talk!</h2>
-                    <div className="">Vivamus in nisl metus? Phasellus.</div>
+
+
+        <section >
+            <div className="container max-w-5xl px-4 py-12 mx-auto">
+                <div className="grid gap-4 mx-4 sm:grid-cols-12">
+                    <div className="col-span-12 sm:col-span-3">
+                        <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-violet-600">
+                            <h3 className="text-3xl font-semibold">Morbi tempor</h3>
+                            <span className="text-sm font-bold tracking-wider uppercase dark:text-gray-600">Vestibulum diam nunc</span>
+                        </div>
+                    </div>
+                    <div className="relative col-span-12 px-4 space-y-6 sm:col-span-9">
+                        <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:dark:bg-gray-300">
+                            <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-violet-600">
+                                <h3 className="text-xl font-semibold tracking-wide">Donec porta enim vel </h3>
+                                <time className="text-xs tracking-wide uppercase dark:text-gray-600">Dec 2020</time>
+                                <p className="mt-3">Pellentesque feugiat ante at nisl efficitur, in mollis orci scelerisque. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                            </div>
+                            <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-violet-600">
+                                <h3 className="text-xl font-semibold tracking-wide">Aliquam sit amet nunc ut</h3>
+                                <time className="text-xs tracking-wide uppercase dark:text-gray-600">Jul 2019</time>
+                                <p className="mt-3">Morbi vulputate aliquam libero non dictum. Aliquam sit amet nunc ut diam aliquet tincidunt nec nec dui. Donec mollis turpis eget egestas sodales.</p>
+                            </div>
+                            <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-violet-600">
+                                <h3 className="text-xl font-semibold tracking-wide">Pellentesque habitant morbi</h3>
+                                <time className="text-xs tracking-wide uppercase dark:text-gray-600">Jan 2016</time>
+                                <p className="mt-3">Suspendisse tincidunt, arcu nec faucibus efficitur, justo velit consectetur nisl, sit amet condimentum lacus orci nec purus. Mauris quis quam suscipit, vehicula felis id, vehicula enim.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <img src="https://i.ibb.co.com/cSFLJ7KF/contact.jpg" alt="" className="p-6 h-52 md:h-64" />
             </div>
-            <form noValidate="" className="space-y-6">
-                <div>
-                    <label htmlFor="name" className="text-sm">Full name</label>
-                    <input id="name" type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-100" />
+
+            <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 ">
+                <div className="flex flex-col justify-between">
+                    <div className="space-y-2">
+                        <h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's talk!</h2>
+                        <div className="">Vivamus in nisl metus? Phasellus.</div>
+                    </div>
+                    <img src="https://i.ibb.co.com/cSFLJ7KF/contact.jpg" alt="" className="p-6 h-52 md:h-64" />
                 </div>
-                <div>
-                    <label htmlFor="email" className="text-sm">Email</label>
-                    <input id="email" type="email" className="w-full p-3 rounded dark:bg-gray-100" />
-                </div>
-                <div>
-                    <label htmlFor="message" className="text-sm">Message</label>
-                    <textarea id="message" rows="3" className="w-full p-3 rounded dark:bg-gray-100"></textarea>
-                </div>
-                <button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-600 dark:text-gray-50">Send Message</button>
-            </form>
-        </div>
+                <form noValidate="" className="space-y-6">
+                    <div>
+                        <label htmlFor="name" className="text-sm">Full name</label>
+                        <input id="name" type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-100" />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="text-sm">Email</label>
+                        <input id="email" type="email" className="w-full p-3 rounded dark:bg-gray-100" />
+                    </div>
+                    <div>
+                        <label htmlFor="message" className="text-sm">Message</label>
+                        <textarea id="message" rows="3" className="w-full p-3 rounded dark:bg-gray-100"></textarea>
+                    </div>
+                    <button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-600 dark:text-gray-50">Send Message</button>
+                </form>
+            </div>
+        </section>
     );
 };
 
